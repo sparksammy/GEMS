4505510638b2058525bbd337b538c08f94f46262
Samuel Lord <sllord@icloud.com>
2020-01-28 13:54:39 -0600

    OK, help would be *greatly* appreciated.
    Still reboots. 0 idea why. Tried just about everything.

--- os.c
+++ os.c
@@ -1,16 +1,38 @@
+//GEMS BY SPARKSAMMY
 #include <stdio.h> //needed for delay and DiamondFS
 #include <stdlib.h> //needed for delay
-#include <time.h> //Time since Jan. 1st 1970
+#include "time.h" //Microsecond time.
 #include "standard_io.h" //I think this might have an issue. Not sure.
 //#include "crashhand.h" //Comming Soon(TM)
-#include "sticky.h"
+//#include "sticky.h" //module deprecated. use new stick function and new function called delay.
 //#include "diamondfs.h" //BORKED - USE AT YOUR OWN RISK
+int sticker = 0;
+
+void stickB() {
+	//do nothing
+}
+
+void stick() {
+	sticker = 1;
+	while(sticker > 0) {
+		stickB();
+	}
+}
+
+void unstick() {
+	sticker = 0;
+}
+
 void kern() {
 	//extern bootloader();
 	//bootloader();
 	clear();
-	print("GEMS OK", 0x10);
+	print("GEMS OK, WAITING 10 SECONDS TO TEST TIME...", 0x10);
+	wait(10000000);
+	print("TIME OK. STARTING TIME SERVICE...", 0x10);
+	count();
 	stick(); //stick when we need it, so we can stick
 	print("CRASH", 0x0f); //This needs to be here and not below because C i guess. This is outter boundries anyways.
 }
+
 //and we all *shut* down...
--- time.h
+++ time.h
@@ -1,1 +1,16 @@
-//Made to nullify unkown code.
+//Made to create time at 1 MHZ (Microseconds)
+//by Sparksammy
+int time = 1;
+
+void count() {
+	while(1) {
+		time++;
+	}
+}
+
+void wait(int microseconds) {
+	int futureTime = microseconds + time;
+	while(futureTime > time) {
+		//do nothing
+	}
+}
