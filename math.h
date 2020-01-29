bbcd0b9950ff29cd4b830c655ba4e7789dbe1054
Oklomsy <32187835+Oklomsy@users.noreply.github.com>
2020-01-29 18:40:10 +0100

    Create math.h

---
+++ math.h
@@ -0,0 +1,12 @@
+
+// A math library for GEMS. work in progress, made by Oklomsy!
+// The math library is a essential part of an OS, and although the languages themselves have math functions. they might not have a function to check if a number is even or odd and it may require some libraries but the math library allows you to save time.
+
+int evenorodd(int n){
+  if (n % 2 == 0){
+    return 2; // 2 = true/even
+  } else {
+    return 1; // 1 = false/odd
+  }
+  return 0;
+}
