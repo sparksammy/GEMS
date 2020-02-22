9ba0f211a13354bad05c4f75b66302949db90669
Oklomsy <oklomsybeta@gmail.com>
2020-02-22 16:21:34 +0100

    Tagon v1.0

--- os.lua
+++ os.lua
@@ -5,15 +5,18 @@ end
 
 -- Tagon is a programming language for GEMS
 -- Since lua running lua doesn't work.
--- also BTW Tagon is a heavily modifed version of the Robolang interpreter although that would work aswell...
 -- Coming soon (TM)
 
 function check(code)
-io.write("Tagon is still in development sorry...")
+cmd = string.lower(code)
+if cmd == "about" then
+io.write("Tagon is a small programming language for GEMS, it is still in early development. it might be discontinued if we do finish making the OS or it might just become a scripting language for GEMS, it only has one command and that's about which prints this message, We might add more commands but Tagon is really not that important but I just MIGHT develop it one day.\n");
+end
 end
 
 io.write("GEMS LOADED SUCCESSFULLY! FINALLY!\n");
 io.write("We didn't plan this far ahead, so feel free to type Tagon code basic style while we create an actual OS..\n");
+io.write("Type about to see more information about Tagon\n");
 while true do
 io.write("LUA> ");
 code = io.read("*l");
