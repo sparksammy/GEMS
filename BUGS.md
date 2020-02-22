e6ac3a7522a97987827f8f26071e7a15bd7fa2a4
Oklomsy <oklomsybeta@gmail.com>
2020-02-22 16:00:29 +0100

    New features...

--- BUGS.md
+++ BUGS.md
@@ -3,7 +3,3 @@ Here is a list of all the bugs:
 1. Restart loop bug.  
 When running GEMS there is a bug that causes the OS to loop.  
 We do not know the origin/source of this bug but it has not been fixed yet.
-
-2. Cannot enter command bug
-When running the os (LUA VERSION) there user cannot type a command  
-because there is a while loop that always resets the command bar.
--- os.lua
+++ os.lua
@@ -1,8 +1,23 @@
-io.write("GEMS LOADED SUCCESSFULLY! FINALLY!");
-io.write("We didn't plan this far ahead, so feel free to type lua code basic style while we create an actual OS..");
+function wait(s)
+  local ntime = os.time() + s
+  repeat until os.time() > ntime
+end
+
+-- Tagon is a programming language for GEMS
+-- Since lua running lua doesn't work.
+-- also BTW Tagon is a heavily modifed version of the Robolang interpreter although that would work aswell...
+-- Coming soon (TM)
+
+function check(code)
+io.write("Tagon is still in development sorry...")
+end
+
+io.write("GEMS LOADED SUCCESSFULLY! FINALLY!\n");
+io.write("We didn't plan this far ahead, so feel free to type Tagon code basic style while we create an actual OS..\n");
 while true do
 io.write("LUA> ");
 code = io.read("*l");
-loadstring(code)
-wait(.1);
+load(code)
+check(code);
+wait(0.1);
 end
