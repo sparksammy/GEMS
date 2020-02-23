358b1ebdaf479a618af858c727e0fab076a8bd21
Oklomsy <oklomsybeta@gmail.com>
2020-02-23 03:17:27 +0100

    One more command

--- os.lua
+++ os.lua
@@ -32,22 +32,29 @@ for token in string.gmatch(line, "[^%s]+") do
 				end
 			end
 		elseif _G.cmd == "version" then
-			io.write("Rushell version 0.5 Beta\n");
+			io.write("Rushell version 0.6 Beta\n");
 		elseif _G.cmd == "help" then
-			io.write("There are six commands:\n");
+			io.write("There are seven commands:\n");
 			io.write("print - Prints to the console - Usage: print hello world\n");
 			io.write("version - prints the version - Usage: version\n");
 			io.write("help - lists all the different commands - Usage: help\n");
 			io.write("relogin - login with a different username - Usage: relogin\n");
 			io.write("clear - clears the screen (not perfect) - Usage: clear\n");
-			io.write("about - About rushell.\n");
+			io.write("about - About rushell. - Usage: about\n");
+			io.write("prompt - Waits until the user presses any key.\n");
 		elseif _G.cmd == "relogin" then
 			io.write("New username: ");
 			usr = io.read("*l");
-		elseif _G.cmd == "clear" or "cls" then
-			clear()
+		elseif _G.cmd == "clear" then
+			clear();
 		elseif _G.cmd == "about" then
 			io.write('Rushell is a small shell/scripting language for the GEMS OS, it was made by Oklomsy and sparksammy with lots of love, it is designed to be simple and delicate. the name comes from the acronym "RUby SHELL" \n');
+		elseif _G.cmd == "wait" then
+			wait(args[1]);
+		elseif _G.cmd == "prompt" then
+			io.write("Press any key to continue");
+			io.read("*l");
+			
 end 
 args = {} --clean up
 
@@ -59,7 +66,6 @@ io.write(usr .. "@Rushell> ")
 line = io.read("*l");
 cmd = string.lower(line)
 rushell(cmd);
-wait(0.1);
 end
 end
 
