3cdb9962c8d37ebb3a9e0296d61a381aa2b196ca
Samuel Lord <sllord@icloud.com>
2020-02-22 20:50:33 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,9 +14,10 @@ echo "Finalizing OS Compile"
 echo "" > "builds/iso/gems.bin"
 dd if="builds/blocks/kernel.bin" >> "builds/iso/gems.bin"
 dd if="builds/blocks/os.bin" >> "builds/iso/gems.bin"
+objcopy --input-target=binary --output-target=elf32-i386  "builds/iso/gems.bin" "builds/iso/gems.elf"
 echo "set default=0" > builds/iso/boot/grub/grub.cfg
 echo "set timeout=0" >> builds/iso/boot/grub/grub.cfg
 echo 'menuentry "GEMS" {' >> builds/iso/boot/grub/grub.cfg
-echo "  multiboot /gems.bin" >> builds/iso/boot/grub/grub.cfg
+echo "  multiboot /gems.elf" >> builds/iso/boot/grub/grub.cfg
 echo "}" >> builds/iso/boot/grub/grub.cfg
 grub-mkrescue -d /usr/lib/grub/i386-pc -o builds/gems.iso builds/iso
