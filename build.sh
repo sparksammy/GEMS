ac66f084a15cf4be3ef57739132c022ff772c600
Samuel Lord <sllord@icloud.com>
2020-03-26 14:32:13 -0500

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+echo "Setting target as 32bit ELF"
+export TARGET=i686-elf
 echo "Making directories"
 mkdir builds
 mkdir builds/blocks
@@ -6,14 +8,10 @@ mkdir builds/iso/boot
 mkdir builds/iso/boot/grub
 echo "Building bootloader"
 nasm -f elf32 -o builds/blocks/bl.o bootloader.asm
-dd if="linker.ld" > "builds/blocks/kernel.bin"
-dd if="builds/blocks/bl.o" >> "builds/blocks/kernel.bin"
 echo "Building OS"
-gcc os.c -g -c -ffreestanding -m32 -o builds/blocks/os.bin -I/home/$USER/lua/src -I/usr/include/lua5.1/ -I/usr/include/x86_64-linux-gnu/ -ldl -llua5.1
+gcc os.c -g -c -ffreestanding -m32 -o builds/blocks/os.o -I"lua/src" -I/usr/include/lua5.1/ -I/usr/include/x86_64-linux-gnu/ -ldl -llua50
 echo "Finalizing OS Compile"
-echo "" > "builds/iso/gems.bin"
-dd if="builds/blocks/kernel.bin" >> "builds/iso/gems.bin"
-dd if="builds/blocks/os.bin" >> "builds/iso/gems.bin"
+ld -m elf_i386 linker.ld builds/blocks/bl.o builds/blocks/os.o -L lua/lib/liblua.a -o builds/iso/gems.bin -Llua/lib -Llua/lib/liblua.a -Llua/lib/liblualib.a -Llua/
 objcopy --input-target=binary --output-target=elf32-i386  "builds/iso/gems.bin" "builds/iso/gems.elf"
 echo "set default=0" > builds/iso/boot/grub/grub.cfg
 echo "set timeout=0" >> builds/iso/boot/grub/grub.cfg
