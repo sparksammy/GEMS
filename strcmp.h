ad2c69aa32e27b38b102a8e9fa48ab9bf32ed2e9
Samuel Lord <sllord@icloud.com>
2020-05-08 20:09:16 -0500

    Add files via upload

---
+++ strcmp.h
@@ -0,0 +1,19 @@
+void strcmpnotequal(char a, char b) {
+	if (a != b) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+void strcmpequal(char a, char b) {
+	if (a == b) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+void strcmpdone() {
+	// TODO
+}
